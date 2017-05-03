a797cd41b60985b23fb484bc7da91bb05f818816
champiiy <albert1047@hotmail.com>
2017-05-03 08:42:04 -0500

    UPDATE V1.4.1

--- vistas/Opusuario.php
+++ vistas/Opusuario.php
@@ -37,8 +37,8 @@
       <aside class="sidebar">
         <ul class="sidebar-nav">
           <li><a href="index.php"><i class="fa fa-dashboard"></i> <span>Index</span></a></li>
-          <li><a href="#configuration" data-toggle="tab"><i class="fa fa-folder-open-o"></i> <span>Catálogos</span></a></li>
-          <li class="active"><a href="#users" data-toggle="tab"><i class="fa fa-archive"></i> <span>Proyectos</span></a></li>
+          <li class="active"><a href="#configuration" data-toggle="tab"><i class="fa fa-folder-open-o"></i> <span>Catálogos</span></a></li>
+          <li><a href="#users" data-toggle="tab"><i class="fa fa-archive"></i> <span>Proyectos</span></a></li>
           <li><a href="#mail" data-toggle="tab"><i class="fa fa-users"></i> <span>Usuarios</span></a></li>
           <li><a href="#mail" data-toggle="tab"><i class="fa fa-cogs"></i> <span>Acerca de</span></a></li>
         </ul>
@@ -47,106 +47,59 @@
       <section class="main">
         <section class="tab-content">
           <section class="tab-pane active" id="configuration">
-           <nav class="subbar">
+          <!--  <nav class="subbar">
               <ul class="nav nav-tabs">
-                <li class="active"><a href="#access" data-toggle="tab"><i class="fa fa-code"></i> <span>Cotización/Proyecto</span></a></li>
-                <li><a href="#roles" data-toggle="tab"><i class="fa fa-user"></i> <span>+ Nueva Cotización/Proyecto</span></a></li>
+                <li class="active"><a href="#access" data-toggle="tab"><i class="fa fa-code"></i> <span>Busqueda Clientes</span></a></li>
+                <li><a href="#roles" data-toggle="tab"><i class="fa fa-user"></i> <span>+ Nuevo Cliente</span></a></li>
               </ul>
-            </nav>
+            </nav> -->
               
             <section class="tab-content content">
               <section class="tab-pane active fade in" id="access">
                 <div class="row">
-                  <div class="col-xs-6 col-sm-3 col-md-9 col-center">
+                  <div class="col-xs-12 col-sm-9 col-md-6">
                     <div class="panel panel-default">
-                      <div class="panel-heading">Tabla de Resultados</div>
-                      <div class="panel-body">
-                        <form>
-                          <div class="row">
-                            <div class="row">
-                              <div class="group">      
-                                <input type="text" required>
-                                <span class="highlight"></span>
-                                <span class="bar"></span>
-                                <label>Titulo</label>
-                              </div>
-
-                              <div class="group">      
-                                <input type="text" required>
-                                <span class="highlight"></span>
-                                <span class="bar"></span>
-                                <label>Cliente</label>
-                              </div>  
-
-                              <div class="group">      
-                                <input type="text" required>
-                                <span class="highlight"></span>
-                                <span class="bar"></span>
-                                <label>Empresa</label>
-                              </div>  
-
-                              <div class="group">      
-                                <input type="date" value="" required>
-                                <span class="highlight"></span>
-                                <span class="bar"></span>
-                                <label>Tipo</label>  
-                              </div><br><br>
-                            </div>
-
-                            <div class="row">
-                              <div class="group">      
-                                <input type="text" required>
-                                <span class="highlight"></span>
-                                <span class="bar"></span>
-                                <label>Fecha Desde</label>
-                              </div>  
-
-                              <div class="group">      
-                                <input type="text" required>
-                                <span class="highlight"></span>
-                                <span class="bar"></span>
-                                <label>Fecha Hasta</label>
-                              </div> 
-
-                              <div class="group left">      
-                                <button class="btn btn-primary adicional">Buscar</button>
-                              </div>  
-                            </div>
-                          </div>  
-                        </form>
-                      </div>
-                    </div>
-                  </div>
-              
-                  <div class="col-xs-12 col-sm-9 col-md-12">
-                    <div class="panel panel-default">
-                      <div class="panel-heading">Tabla de Resultados</div>
+                      <div class="panel-heading">Proyectos Responsable</div>
                       <div class="panel-body">
                         <table class="table table-striped table-hover">
                           <thead>
                             <tr class="table-color">
-                            <th>#</th>
-                            <th>Titulo/Descripción</th>
-                            <th>Monto</th>
-                            <th>Cliente</th>
-                            <th>Empresa</th>
-                            <th>Tipo</th>
-                            <th>Fechas</th>
-                            <th>Estatus</th>
-                            <th>Opciones</th>
-                          </tr>
+                              <th>Fecha</th>
+                              <th>Titulo</th>
+                            </tr>
                           </thead>
                           <tbody>
                             <tr class="text-left">
-                              <td>1</td>
-                              <td>Jhon Doe</td>
-                              <td>12312312312</td>
-                              <td>9992786534</td>
-                              <td>rh@hotmail.com</td>
-                              <td>Calle 16, Col. Yucatan</td>
-                              <td>Calle 16, Col. Yucatan</td>
-                              <td>Calle 16, Col. Yucatan</td>
-                              <td>Eliminar</td>
+                              <td>0</td>
+                              <td>0</td>
+                            </tr>
+                            <tr class="text-left">
+                              <td>Mostrando: 0</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="col-xs-12 col-sm-9 col-md-6">
+                    <div class="panel panel-default">
+                      <div class="panel-heading">Tareas Asignadas</div>
+                      <div class="panel-body">
+                        <table class="table table-striped table-hover">
+                          <thead>
+                            <tr class="table-color">
+                              <th>Fecha</th>
+                              <th>Titulo</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr class="text-left">
+                              <td>0</td>
+                              <td>0</td>
+                            </tr>
+                            <tr class="text-left">
+                              <td>Mostrando: 0</td>
                             </tr>
                           </tbody>
                         </table>
@@ -275,8 +228,9 @@
                                 <button class="btn btn-primary adicional">Enviar</button>
                               </div>
 
-                              <div class="group b">      
-                                <button class="btn btn-primary adicional">Cancelar</button>
+                              <div class="group b">
+                                <a href="#" class="btn btn-primary adicional">Cancelar</a>   
+                                <!-- <button class="btn btn-primary adicional">Cancelar</button> -->
                               </div>
                             </div>
                           </div>
